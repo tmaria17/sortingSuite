ab3420499bae47c4adba048e1fccb71176405aa4
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-20 23:23:32 -0700

    Add some more testing.

--- test/bubbleSort-test.js
+++ test/bubbleSort-test.js
@@ -2,9 +2,13 @@ const assert = require('chai').assert;
 const bubbleSort = require('../bubbleSort');
 
 describe('bubble sort functionality', function(){
+  it('is a function', function(){
+    assert.isFunction(bubbleSort)
+  });
   it('can sort an array in ascending order', function(){
     nums = [2, 5, 3, 1, 4, 7, 8, 6, 9]
     expectedNums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     assert.deepEqual(bubbleSort(nums), expectedNums)
   });
+
 });
