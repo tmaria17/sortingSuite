b20c2846e7b1050e6d4e483538885b5ad833aefa
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-20 23:24:18 -0700

    Make small change to testing array.

--- test/bubbleSort-test.js
+++ test/bubbleSort-test.js
@@ -6,8 +6,8 @@ describe('bubble sort functionality', function(){
     assert.isFunction(bubbleSort)
   });
   it('can sort an array in ascending order', function(){
-    nums = [2, 5, 3, 1, 4, 7, 8, 6, 9]
-    expectedNums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    nums = [10, 2, 5, 3, 1, 4, 7, 8, 6, 9]
+    expectedNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     assert.deepEqual(bubbleSort(nums), expectedNums)
   });
 
